2266e29efc1f21e1729a3a1e84775059b3e57e7c
Andrey Vorontsov <andr13b@yandex.ru>
2021-10-02 16:39:43 +0300

    sounds content

---
+++ Assets/Scripts/Storage/Audio/SoundPack.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Storage.Audio
+{
+    [Serializable]
+    public class SoundPack
+    {
+        public string name;
+        public List<Sound> sounds;
+    }
+}
