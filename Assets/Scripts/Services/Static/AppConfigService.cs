31bb49d005f9f9d9dbe668a81e1bec720d08826c
ilya pribil <ilya@casualgamesonline.com>
2026-01-27 14:08:30 +0500

    Config To Debug Version

--- Assets/Scripts/Services/Static/AppConfigService.cs
+++ Assets/Scripts/Services/Static/AppConfigService.cs
@@ -3,7 +3,7 @@ namespace Services.Static
 {
     public static class AppConfigService
     {
-        private static readonly Environment _currentEnv = Environment.Production;
+        private static readonly Environment _currentEnv = Environment.Debugs;
         
         public static string Version = "1.0.0";
         
